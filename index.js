e1c1503e05187c307f914a73ed8488933709f465
Jennie <jentdev@gmail.com>
2023-09-30 21:24:33 -0700

    made changes to connectdb

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-// export function to connect to mongodb
-const connectDB = require('./config/db');
-
-// load config
-dotenv.config({ path: './config/config.env'});
-
-connectDB();
-
-// db.on('error', (err) => console.error(err));
-// db.once('connected', () => console.log(`connect to db.`));
-
-const app = express();
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 
 // middleware
 app.use(express.json()); // accept json as body
@@ -23,4 +22,7 @@ app.use(express.json()); // accept json as body
 // routes
 app.use('/scores', require('./routes/scores'));
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+})
