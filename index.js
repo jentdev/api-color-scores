399f637d88cd031633d11019d10194741f76642c
Jennie <jentdev@gmail.com>
2023-10-01 20:57:44 -0700

    fixed cors error

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 const connectDB = async () => {
